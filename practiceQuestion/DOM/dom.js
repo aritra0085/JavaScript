3485b644f187233a09dfadd4a6059b5eb9bc9b4d
aritra0085 <aritrabiswas0085@gmail.com>
2025-04-27 20:36:04 +0530

    dom enter

---
+++ practiceQuestion/DOM/dom.js
@@ -0,0 +1,59 @@
+
+
+//the process of accesesing html code in the javaScript is called DOM
+
+
+// console.log("hello!");
+// // alert("hi aritra from js");
+// // console.log(window)
+// window.console.log("hello2");
+
+// console.dir(window);
+// console.dir(window.document);
+// console.dir(document); //alternative of console.dir(window.document);
+// console.dir(document.body);
+// console.log(document.body);
+
+// console.dir(document.body.childNodes[1]);
+
+// //use of js for acsessing the elements.
+// // document.body.childNodes[3].innerText = "abcd";
+
+// let heading = document.getElementById("heading");
+// console.dir(heading); /*useing of id*/
+
+// let headings = document.getElementsByClassName("heading-class");
+// console.dir(headings);
+// console.log(headings);
+
+// let button = document.getElementById("myid");
+// console.dir(button);
+
+// let parahs = document.getElementsByTagName('p');
+// console.dir(parahs);
+
+// //Querey selctor tag
+// let firstelem = document.querySelector('p'); //1st element
+// console.dir(firstelem);
+
+// let allelemnts = document.querySelectorAll('p'); //all element
+// console.dir(allelemnts);
+
+// let allclass = document.querySelectorAll('.heading-class'); //all element
+// console.dir(allclass); //class selection
+
+// let allid = document.querySelectorAll('#myid'); //all element
+// console.dir(allid); //Id selection
+
+// //children justify in a dom 
+
+// console.dir(document.body.firstChild); //dom tree default is (text) but there are three sectors that are text , console , element
+
+// // document.querySelector("div");
+// // document.querySelector("div").children;
+// //use in console
+
+// // this are use for navigation
+
+let div = document.querySelector("div");
+    console.dir(div);
